ee9e53acb10c4d184b2f84aaca3a95a2d8965f0f
Lauren Bentley <laurendebentley@gmail.com>
2023-03-31 11:44:49 -0400

    sign in button added to nav

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,12 +3,16 @@ import icon from '../assets/tomato-24.svg'
 
 export default function Header() {
     return (
+    <div class='container'>
       <nav className="navbar">
+        
         <a className="navbar-brand" href="#">
           <img src={icon} />
           TomatoDuck
         </a>
+        <button class='login-btn'>Sign In</button>
       </nav>
+      </div>
     );
   }
   
