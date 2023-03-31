97a29609679647115f65ba47477367ea6c5e2036
Lauren Bentley <laurendebentley@gmail.com>
2023-03-31 11:29:14 -0400

    svg tomato icon added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,12 @@
 import react from 'react'
+import icon from '../assets/tomato-24.svg'
 
 export default function Header() {
     return (
       <nav className="navbar">
         <a className="navbar-brand" href="#">
-          <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" className="d-inline-block align-top" alt="" />
-          Bootstrap
+          <img src={icon} />
+          TomatoDuck
         </a>
       </nav>
     );
