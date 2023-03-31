7110ee2e09fcda9ff3b9eaf4f3d5224a90cb845f
Lauren Bentley <laurendebentley@gmail.com>
2023-03-31 11:22:33 -0400

    navbar/header started

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
 import Timer from './components/Timer'
+import Header from './components/Header'
 // import bootstrap from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
         <Timer />
       
     </div>
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,13 @@
 import react from 'react'
 
-// export default function Header() {
-//     return(
-        
-//     )
-// }
+export default function Header() {
+    return (
+      <nav className="navbar">
+        <a className="navbar-brand" href="#">
+          <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" className="d-inline-block align-top" alt="" />
+          Bootstrap
+        </a>
+      </nav>
+    );
+  }
+  
