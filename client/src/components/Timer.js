4205cd963499765ce1d139e0cc1c1a22baf6baaf
Lauren Bentley <laurendebentley@gmail.com>
2023-03-31 10:33:55 -0400

    timer component layout, pre-final style

--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -1,9 +1,18 @@
 import react from 'react'
 
-export default function Timer() {
-    return(
-        <div class='container'>
-            <p>Hello</p>
+export default function Timer(){
+    return (
+        <div className="container timer-box col-5 d-flex-col justify-content-center">
+            <div className="timer-settings d-flex justify-content-center">
+                <button className="m-4">Short Break</button>
+                <button className="m-4">Long Break</button>
+                <button className="m-4">Tomato Time</button>
+            </div>
+            <div className="timer container col-6 d-flex justify-content-center bordered">
+                <h1 className='my-5'>45:00</h1>
+            </div>
         </div>
     )
 }
+
+
