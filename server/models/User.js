054f10d21f6899549ea50872c39f6fe61bd8afb5
Ivan Escobar <ivnescbr1989@gmail.com>
2023-04-01 17:47:27 -0400

    user model

---
+++ server/models/User.js
@@ -0,0 +1,38 @@
+const {Schema, model} = require(`mongoose`)
+const bcrypt = require(`bcrypt`)
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, `Please enter VALID!!! email address`]
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        todos: [todoSchema]
+    },
+    // {
+    //     toJSON: {
+    //         virtuals: true
+    //     }
+    // }
+)
+ userSchema.pre(`save`, async (next) => {
+    if(this.isNew || this.isModified(`password`)) {
+        const saltRounds = 10 //bcrypt option to set `cost factor`, higher the value longer the processing time
+        this.password = await bcrypt.hash(this.password, saltRounds)
+    }
+    next()
+ })
+
+ userSchema.methods.verifyPassword = async (password) => bcrypt.compare(password, this.password)
+
