653c7df4004e40f2ada9f23afeb741a5ed7b209e
Ivan Escobar <ivnescbr1989@gmail.com>
2023-04-07 15:25:52 -0400

    signup/sigin modal

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 import icon from '../assets/tomato-24.svg';
-import { Modal, ModalHeader, ModalBody, ModalFooter, Input,InputGroup, InputGroupText } from 'reactstrap';
+// import { Nav, Container} from 'reactstrap';
+import { Modal, Tab, Nav,} from 'react-bootstrap'
+import SignUpForm from './signup';
+import LoginForm from './login';
 // import Auth from '../utils/auth'
 
 const Header = () => {
   const [modal, setModal] = useState(false);
 
-  const toggleModal = () => {
-    setModal(!modal);
-  };
+  // const toggleModal = () => {
+  //   setModal(!modal);
+  // };
 
   return (
     <div className='container'>
@@ -17,37 +20,41 @@ const Header = () => {
           <img src={icon} alt='Tomato icon' />
           TomatoDuck
         </a>
-        <button className='login-btn' onClick={toggleModal}>
-          Sign In
+        <button className='login-btn' onClick={()=>setModal(true)}>
+          Sign In/Sign Up
         </button>
       </nav>
-      <Modal isOpen={modal} toggle={toggleModal}>
-        <ModalHeader toggle={toggleModal}>Sign In</ModalHeader>
-        <ModalBody>
-        <div>
-  <InputGroup>
-    
-    <Input placeholder="username" />
-  </InputGroup>
-  <br />
-  <InputGroup>
-    
-    <Input placeholder="password" />
-  </InputGroup>
-  <br />
-  
-  <br />
-  
-</div>
-        </ModalBody>
-        <ModalFooter>
-          <button color='secondary' onClick={toggleModal}>
-            Close
-          </button>
-          <button color='primary' onClick={toggleModal}>
-            Sign In
-          </button>
-        </ModalFooter>
+
+
+     <Modal
+      show={modal}
+      onHide={()=> setModal(false)}
+      aria-labelledby='signup-modal'>
+        <Tab.Container defaultActiveKey ='login'>
+          <Modal.Header closeButton>
+            <Modal.Title id='signup-modal'>
+              <Nav variant='pills'>
+                <Nav.Item>
+                  <Nav.Link eventKey='login'>Sign In</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Tab.Content>
+              <Tab.Pane eventKey='login'>
+                <LoginForm handleModalClose={()=> setModal(false)}/>
+              </Tab.Pane>
+              <Tab.Pane eventKey='signup'>
+                <SignUpForm handleModalClose={()=> setModal(false)}/>
+              </Tab.Pane>
+            </Tab.Content>
+          </Modal.Body>
+        </Tab.Container>
+
       </Modal>
     </div>
   );
