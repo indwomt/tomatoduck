155cf72598ee6a1f0596678276f4895888881661
Lauren Bentley <laurendebentley@gmail.com>
2023-04-08 16:24:57 -0400

    server working on local machine post-postman update, task-card subcomponent started

--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -1,42 +1,46 @@
-import react,{useState} from 'react'
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-
+import Form from 'react-bootstrap/Form';
+import TaskCard from './task-card'
 
 export default function Tasks() {
-
-    const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-    return(
-        <div className='container col-8 d-flex-col justify-content-center my-5'>
-            <div className='task-header col-12 justify-content-between d-flex flex-wrap'>
-                {/* add something like todo[i].length to get the tasks needed */}
-                <h2 class='col-md-4 flex-wrap'>Tasks Left: 0</h2>
-                <button className='add-task-btn col-md-4 flex-wrap' onClick={handleShow}>Add Task</button>
-            </div>
-            <div className='task-container'>
 
-            </div>
-
+  return (
+    <div className='container col-8 d-flex-col justify-content-center my-5'>
+      <div className='task-header col-12 justify-content-between d-flex flex-wrap'>
+        {/* add something like todo[i].length to get the tasks needed */}
+        <h2 class='col-md-4 flex-wrap'>Tasks Left: 0</h2>
+        <button className='add-task-btn col-md-4 flex-wrap' onClick={handleShow}>
+          Add Task
+        </button>
+      </div>
+      <div className='task-container'>
+        <TaskCard />
+      </div>
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Add a new task</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
+        <Form>
+          <Form.Group className='mb-3' controlId='formBasicEmail'>
+            <Form.Control type='email' placeholder='Your To-do Task' />
+          </Form.Group>
+          <Modal.Footer>
+            <Button variant='secondary' onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant='primary' onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Form>
       </Modal>
-        </div>
-        
-    )
-
+    </div>
+  );
 }
---
+++ client/src/components/task-card.js
@@ -0,0 +1,13 @@
+import react, {useEffect, useState} from 'react';
+import Card from 'react-bootstrap/Card';
+
+export default function taskCard(props) {
+
+    return(
+        <Card>
+            <Card.Body>I am a task to do!!!</Card.Body>
+        </Card>
+    )
+
+
+}
