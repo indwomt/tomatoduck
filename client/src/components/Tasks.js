d07df31d924435a938b298683aa38e3b08653dc9
Lauren Bentley <laurendebentley@gmail.com>
2023-04-08 12:26:55 -0400

    new task modal

--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -1,16 +1,42 @@
-import react from 'react'
+import react,{useState} from 'react'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 
 export default function Tasks() {
+
+    const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
     return(
-        <div className='container col-4 d-flex-col justify-content-center my-5'>
+        <div className='container col-8 d-flex-col justify-content-center my-5'>
             <div className='task-header col-12 justify-content-between d-flex flex-wrap'>
-                <h2 class='col-12 col-md-6 flex-wrap'>Tasks Left: 0</h2>
-                <button className='add-task-btn col-md-6 col-12 flex-wrap'>Add Task</button>
+                {/* add something like todo[i].length to get the tasks needed */}
+                <h2 class='col-md-4 flex-wrap'>Tasks Left: 0</h2>
+                <button className='add-task-btn col-md-4 flex-wrap' onClick={handleShow}>Add Task</button>
             </div>
             <div className='task-container'>
 
             </div>
+
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
         </div>
+        
     )
 
 }
