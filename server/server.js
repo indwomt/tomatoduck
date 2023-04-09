4eabc1623bbc19d2d33a081c8def98ae325cec9e
Ivan Escobar <ivnescbr1989@gmail.com>
2023-04-09 18:39:31 -0400

    reworking routes

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const db = require('./config/connection')
 const routes = require(`./routes`)
 
+const path = require('path');
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -15,9 +16,6 @@ app.use(express.json())
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-app.get(`*`, (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
 
 app.use(routes)
 
