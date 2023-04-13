461a21cd78b3fbc665e1a91fd465dbed5e9a0ca5
Lauren Bentley <laurendebentley@gmail.com>
2023-04-13 09:03:41 -0400

    task, delete-btn styling

--- client/src/components/task-card.js
+++ client/src/components/task-card.js
@@ -19,14 +19,26 @@ const deleteTask = async (id) => {
     }
 }
 return(
-        <div className="container col-6">
-            
-                {user
-                    ? user.map(tasks => (<Card key={tasks._id} className='mt-2'>{tasks.todo} <button onClick={() => deleteTask(tasks._id)}>delete</button></Card>))
-                    : <Card>No Tasks Saved Yet...</Card>
-                    }
-
-        </div>
+    <div className="container col-10">
+    {user
+      ? user.map(tasks => (
+        <Card key={tasks._id} className='task-box col-12 mt-2'>
+          <div className="d-flex justify-content-between">
+            <div className="col my-3 px-3 align-items-center">
+              <p className='align-items-center task-text'>{tasks.todo}</p>
+            </div>
+            <div className="col-auto p-2 align-items-center">
+            <Button className='delete-btn' onClick={() => deleteTask(tasks._id)}>
+  <Trash />
+</Button>
+            </div>
+          </div>
+        </Card>
+      ))
+      : <Card>No Tasks Saved Yet...</Card>
+    }
+  </div>
+  
     )
 }
 
