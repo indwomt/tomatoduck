561069ce17a16374a863a8893ef7ff920a928d16
Ivan Escobar <ivnescbr1989@gmail.com>
2023-06-22 12:17:03 -0400

    local storage task, infinite render fix

--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -3,7 +3,7 @@ import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
 import TaskCard from "./Task-card";
-import TaskNoSignup from "./Task-no-signup";
+// import TaskNoSignup from "./Task-no-signup";
 import Auth from "../utils/auth";
 import { saveTodo, getMe } from "../utils/API";
 import { deleteTodo } from "../utils/API";
@@ -21,8 +21,9 @@ export default function Tasks() {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
     if (!token) {
       const todoArray = JSON.parse(localStorage.getItem(`todos`));
-const newArray = todoArray.filter(todoArray=> todoArray !== id)
-localStorage.setItem(`todos`, JSON.stringify(newArray))
+      const newArray = todoArray.filter((todoArray) => todoArray !== id);
+      localStorage.setItem(`todos`, JSON.stringify(newArray));
+      setTask(newArray)
     } else {
       try {
         const del = await deleteTodo(id, token);
@@ -36,27 +37,47 @@ localStorage.setItem(`todos`, JSON.stringify(newArray))
       }
     }
   };
+
   const handleInput = (e) => {
     const { name, value } = e.target;
     setTodo({ todos, [name]: value });
   };
+
+  const todosLocalStorage = JSON.parse(localStorage.getItem(`todos`)) || [];
+  const [task, setTask] = useState("");
+
   const handleFormSubmit = async (event) => {
     event.preventDefault();
     const token = Auth.loggedIn() ? Auth.getToken() : null;
     if (!token) {
-      return false;
-    }
-    try {
-      const response = await saveTodo(todos, token);
-      if (!response.ok) {
-        throw new Error(`there's trouble`);
+      todosLocalStorage.push(task);
+      localStorage.setItem("todos", JSON.stringify(todosLocalStorage));
+      setTask(``);
+    } else {
+      try {
+        const response = await saveTodo(todos, token);
+        if (!response.ok) {
+          throw new Error(`there's trouble`);
+        }
+        setTodo({ todo: `` });
+      } catch (error) {
+        console.error(error);
       }
-      setTodo({ todo: `` });
-    } catch (error) {
-      console.error(error);
     }
   };
+
   const userArray = userData.todos;
+
+  const [todosLocal, setTodosLocal] = useState([]);
+
+  // const todoArray = JSON.parse(localStorage.getItem(`todos`));
+  useEffect(() => {
+    const getLocalStorage = () => {
+      const todosUpdate = JSON.parse(localStorage.getItem(`todos`));
+      setTodosLocal(todosUpdate);
+    };
+    getLocalStorage();
+  }, [task]);
 
   //use effect grabs user object from db in order to acces todo array fro our saved tasks
   useEffect(() => {
@@ -80,7 +101,7 @@ localStorage.setItem(`todos`, JSON.stringify(newArray))
     };
     getUser();
   }, [todos]);
-  
+
   return (
     <div className=" task-section col-12 d-flex-col my-5">
       <div className="task-header col-12 justify-content-between d-flex flex-wrap">
@@ -96,7 +117,11 @@ localStorage.setItem(`todos`, JSON.stringify(newArray))
         </button>
       </div>
       <div className="task-container">
-        <TaskCard user={userArray} deleteTask={deleteTask} />
+        <TaskCard
+          user={userArray}
+          deleteTask={deleteTask}
+          todoLocal={todosLocal}
+        />
       </div>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
@@ -123,7 +148,25 @@ localStorage.setItem(`todos`, JSON.stringify(newArray))
             </Modal.Footer>
           </Form>
         ) : (
-          <TaskNoSignup close={handleClose} />
+          <Form onSubmit={handleFormSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control
+                type="text"
+                placeholder="Your To-do Task"
+                name="todo"
+                onChange={(e) => setTask(e.target.value)}
+                value={task}
+              />
+            </Form.Group>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button disabled={!task} variant="primary" type="submit">
+                Save Changes
+              </Button>
+            </Modal.Footer>
+          </Form>
         )}
       </Modal>
     </div>
--- client/src/components/task-card.js
+++ client/src/components/task-card.js
@@ -1,56 +1,48 @@
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button'
-import {Trash} from 'react-bootstrap-icons'
-import { useEffect, useState } from 'react';
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { Trash } from "react-bootstrap-icons";
+// import { useEffect, useState } from "react";
 
-export default function TaskCard({user, deleteTask}) {
-  const [todos, setTodos] = useState([])
-  useEffect(() => {
+export default function TaskCard({ todoLocal, user, deleteTask }) {
 
-  const todosUpdate = JSON.parse(localStorage.getItem(`todos`))
-  setTodos(todosUpdate)
-
-  },[todos]
-  )
-return(
+  return (
     <div className="container col-10">
-    {user
-      ? user.map(tasks => (
-        <Card key={tasks._id} className='task-box col-12 mt-2'>
-          <div className="d-flex justify-content-between">
-            <div className="col my-3 px-3 align-items-center">
-              <p className='align-items-center task-text'>{tasks.todo}</p>
+      {user ? (
+        user.map((tasks) => (
+          <Card key={tasks._id} className="task-box col-12 mt-2">
+            <div className="d-flex justify-content-between">
+              <div className="col my-3 px-3 align-items-center">
+                <p className="align-items-center task-text">{tasks.todo}</p>
+              </div>
+              <div className="col-auto p-2 align-items-center">
+                <Button
+                  className="delete-btn"
+                  onClick={() => deleteTask(tasks._id)}
+                >
+                  <Trash />
+                </Button>
+              </div>
             </div>
-            <div className="col-auto p-2 align-items-center">
-            <Button className='delete-btn' onClick={() => deleteTask(tasks._id)}>
-              <Trash />
-            </Button>
+          </Card>
+        ))
+      ) : todoLocal ? (
+        todoLocal.map((task) => (
+          <Card key={task} className="task-box col-12 mt-2">
+            <div className="d-flex justify-content-between">
+              <div className="col my-3 px-3 align-items-center">
+                <p className="align-items-center task-text">{task}</p>
+              </div>
+              <div className="col-auto p-2 align-items-center">
+                <Button className="delete-btn" onClick={() => deleteTask(task)}>
+                  <Trash />
+                </Button>
+              </div>
             </div>
-          </div>
-        </Card>
-      ))
-      : todos
-      ? todos.map(task => (
-        <Card key={task} className='task-box col-12 mt-2'>
-          <div className="d-flex justify-content-between">
-            <div className="col my-3 px-3 align-items-center">
-              <p className='align-items-center task-text'>{task}</p>
-            </div>
-            <div className="col-auto p-2 align-items-center">
-            <Button className='delete-btn' onClick={() => deleteTask(task)}>
-              <Trash />
-            </Button>
-            </div>
-          </div>
-        </Card>
-      ))
-      
-      :<Card>No Tasks Saved Yet...</Card>
-    }
-  </div>
-  
-    )
+          </Card>
+        ))
+      ) : (
+        <Card>No Tasks Saved Yet...</Card>
+      )}
+    </div>
+  );
 }
-
-
-
